bec2fbde3bf9ad486596e1bf673b376283fa349e
tuananh123984 <166016866+tuananh1234984@users.noreply.github.com>
2025-04-19 01:13:10 +0700

    feat: thêm Menu

--- restaurant-app/src/main/java/com/restaurent/restaurant_app/model/Menu.java
+++ restaurant-app/src/main/java/com/restaurent/restaurant_app/model/Menu.java
@@ -1,5 +1,45 @@
 package com.restaurent.restaurant_app.model;
 
 public class Menu {
-    
+    private String name;
+    private String description;
+    private double price;
+
+    public Menu(String name, String description, double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    @Override
+    public String toString() {
+        return "Menu{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
